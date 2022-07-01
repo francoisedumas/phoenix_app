1968546755928210518c4465f314bad4baa52179
Francois Dumas <dumaslattaque.francois@gmail.com>
2022-07-01 19:09:21 +0200

    Feat - Setup HP image and text

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -5,9 +5,6 @@ export default class extends Controller {
   static targets = ["content"];
   static values = { class: String }
 
-  connect() {
-    console.log("hello")
-  }
   toggle() {
     this.contentTargets.forEach( (content) => content.classList.toggle(this.classValue) )
   }
